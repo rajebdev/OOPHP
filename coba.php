b2ee68a121cdd4ad0e2ed6d21e3e9d70e4928e95
rajebdev <wijanarko.rajeb@gmail.com>
2019-06-28 10:35:15 +0700

    pertemuan 2 - class dan object

---
+++ coba.php
@@ -0,0 +1,19 @@
+<?php 
+
+// coba
+class Coba
+{
+    
+}
+
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+?>
---
+++ produk.php
@@ -0,0 +1,18 @@
+<?php 
+
+// Jualan Produk
+// Komik
+// Game
+
+
+class Produk{
+
+}
+
+
+$prod1 = new Produk();
+$prod2 = new Produk();
+
+
+
+?>
