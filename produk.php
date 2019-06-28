d92eeb3115f8bbf7026160262566ffdd87bd9a95
rajebdev <wijanarko.rajeb@gmail.com>
2019-06-28 11:06:01 +0700

    pertemuan 3 - property & method

--- produk.php
+++ produk.php
@@ -28,20 +28,20 @@ class Produk{
 // var_dump($prod2);
 
 
-$produk3 = new Produk();
-$produk3->judul = "Naruto";
-$produk3->penulis = "Masashi Kishimoto";
+$produk3           = new Produk();
+$produk3->judul    = "Naruto";
+$produk3->penulis  = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
-$produk3->harga = 30000;
+$produk3->harga    = 30000;
 
 // echo $produk3->getLabel();
 // echo "<br>";
 
-$produk4 = new Produk();
-$produk4->judul = "Uncharted";
-$produk4->penulis = "Neil Druckman";
+$produk4           = new Produk();
+$produk4->judul    = "Uncharted";
+$produk4->penulis  = "Neil Druckman";
 $produk4->penerbit = "Sony Computer";
-$produk4->harga = 250000;
+$produk4->harga    = 250000;
 
 echo "Komik : ".$produk3->getLabel();
 echo "<br>";
